c4f397fc73a90911605f325c254a04a589f9be4b
Areej Abbasi <areejabbasiasa@gmail.com>
2025-11-29 22:41:34 -0800

    Create chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,15 @@
+def chatbot():
+    while True:
+        msg = input("You: ").lower()
+
+        if msg == "hello":
+            print("Bot: Hi!")
+        elif msg == "how are you":
+            print("Bot: I'm fine, thank you!")
+        elif msg == "bye":
+            print("Bot: Goodbye!")
+            break
+        else:
+            print("Bot: I don't understand.")
+
+chatbot()
